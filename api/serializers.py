ac6297e3f73460e53d8cff2b450e26f047f5a5bc
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2024-06-10 00:33:42 +0300

    created api urls and simple errors

--- api/serializers.py
+++ api/serializers.py
@@ -2,12 +2,12 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializers):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields =["id","username","password"]
         extra_kwargs = {"password":{"write_only":True}}
 
-    def create(self, validate_data):
-        user = User.objects.create_user(**validate_data)
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
         return user
