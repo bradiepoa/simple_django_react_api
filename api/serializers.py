438927307393eb06ee369d1c92f2c8964f90e414
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2024-06-10 09:31:31 +0300

    updatd edit serializer

--- api/serializers.py
+++ api/serializers.py
@@ -20,8 +20,20 @@ class NoteSerializer(serializers.ModelSerializer):
         # to no allow writting user
         extra_kwargs = {"author": {"read_only":True}}
 
+
 class NoteEditSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
         fields = ["id", "title", "content"]
         read_only_fields = ["id", "author", "created_at"]
+
+    def validate_title(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError("Title must be at least 5 characters long.")
+        return value
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.save()
+        return instance
